67516514628a68e88196e4c9fc5b9150c48fa156
mac <mac@macs.local>
2020-07-14 09:52:46 +0100

    my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/gee.jpg')
+              ),
+              Text(
+                'Glory Mpamah',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+2348033293509',
+                    style: TextStyle(
+                        color: Colors.teal.shade900
+                    ),
+                  ),
+                ),
+                  ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
+              leading: Icon(
+                Icons.mail,
+                color: Colors.teal,
+              ),
+              title: Text(
+                'glorympamah45@gmail.com',
+                style: TextStyle(
+                    color: Colors.teal.shade900
+                ),
+              ),
+            ),
+              ),
+            ],
+          ),
+
+          ),
+        ),
+
+        );
+
+  }
 }
